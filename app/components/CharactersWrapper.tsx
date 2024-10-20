853e9bf50c35f65a1b14686aa965addc91bfe3c8
JMohammad Alzeer <mohammadalzeer400@gmail.com>
2024-10-20 23:29:41 +0300

    Tiny font fix

--- app/components/CharactersWrapper.tsx
+++ app/components/CharactersWrapper.tsx
@@ -9,7 +9,7 @@ const CharactersWrapper: React.FC<CharactersWrapperProps> = (props) => {
   return (
     <>
       <main className="w-[75%] p-4 min-h-screen mb-14 ">
-        <h2 className="mb-4 text-[8px] font-bold text-blue-400 md:text-xl lg:text-2xl">
+        <h2 className="mb-4 text-[8px] sm:text-lg font-bold text-blue-400 md:text-xl lg:text-2xl">
           Characters
         </h2>
         <div className="grid grid-cols-5 relative gap-4  ">
