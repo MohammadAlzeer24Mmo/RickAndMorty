5066253bb52c20a5ea45bba0c99d85f9bef77e91
JMohammad Alzeer <mohammadalzeer400@gmail.com>
2024-10-20 22:56:13 +0300

    responsiveness fixes

--- app/components/EpisodeCharacters.tsx
+++ app/components/EpisodeCharacters.tsx
@@ -82,7 +82,7 @@ const EpisodeCharacters: React.FC<EpisodeCharactersProps> = (props) => {
         </p>
       )}
       {/* Rendering Characters for the selected episode */}
-     {props.episodeIdNumber && <div className="fixed font-bold text-blue-400 left-[40%] md:left-[44%]  lg:left-[40%] text-[8px] md:text-xl lg:text-2xl top-[16px]">
+     {props.episodeIdNumber && <div className="absolute font-bold text-blue-400 left-[13%] md:left-[20%]  lg:left-[20%] text-[8px] md:text-xl lg:text-2xl top-[-28px] md:top-[-44px] lg:top-[-47px]">
         ( {episodeName} )
       </div>} 
       {!loadingEpisodeCharacters && props.episodeIdNumber &&
--- app/components/Episodes.tsx
+++ app/components/Episodes.tsx
@@ -65,7 +65,7 @@ const Episodes: React.FC<EpisodesProps> = (props) => {
                 href="#"
                 className={`font-style text-center transform transition-all duration-300 ${
                   episodeID === episode.id ? "bg-blue-700 scale-105 shadow-lg" : ""
-                } block p-3 rounded border-2 border-blue-400 hover:bg-blue-600 hover:scale-105 hover:shadow-lg text-[6px] sm:text-sm lg:text-base`}
+                } block p-1 md:p-3 rounded border-2 border-blue-400 hover:bg-blue-600 hover:scale-105 hover:shadow-lg text-[6px] sm:text-xs md:text-sm lg:text-base`}
               >
                 {episode.name}
               </a>
