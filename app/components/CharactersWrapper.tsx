82184691d238df15cd26345e9a1ce7ef676938f5
JMohammad Alzeer <mohammadalzeer400@gmail.com>
2024-10-20 21:06:46 +0300

    replacing sticky position for pagination with absolute position to support safari browser

--- app/components/CharactersWrapper.tsx
+++ app/components/CharactersWrapper.tsx
@@ -8,11 +8,11 @@ interface CharactersWrapperProps {
 const CharactersWrapper: React.FC<CharactersWrapperProps> = (props) => {
   return (
     <>
-      <main className="w-[75%] relative p-4 min-h-screen mb-12 ">
+      <main className="w-[75%] p-4 min-h-screen mb-14 ">
         <h2 className="mb-4 text-[8px] font-bold text-blue-400 md:text-xl lg:text-2xl">
           Characters
         </h2>
-        <div className="grid grid-cols-5 gap-4  ">
+        <div className="grid grid-cols-5 relative gap-4  ">
           {props.children}
         </div>
       </main>
--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Pagination: React.FC = () => {
   return (
-    <div className="flex items-center sticky left-[55.6%] top-[500px] justify-center">
+    <div className="flex items-center absolute left-0 right-0 bottom-[-54px] justify-center">
       <div className="flex items-center space-x-2">
         <button className="pagination-arrow">
           ←
